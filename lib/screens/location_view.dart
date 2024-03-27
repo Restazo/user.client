d57b7b8f86c91333e73c7012cc7e1e12a69f7630
Hamidreza Yourdkhani <hamidreza.yourdkhani1993@gmail.com>
2024-03-27 22:42:39 +0200

    First interaction screen UI

--- lib/screens/location_view.dart
+++ lib/screens/location_view.dart
@@ -1,168 +1,166 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+// import 'package:location/location.dart';
+// import 'package:permission_handler/permission_handler.dart' as ph;
 
-// LocationView displays location-related information and options to the user
 class LocationView extends StatelessWidget {
+  const LocationView({super.key});
+
+  Widget _buildSection(
+    BuildContext context,
+    String title,
+    String content,
+    String imagePath,
+  ) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Image.asset(
+              imagePath,
+              height: 16,
+            ),
+            const SizedBox(width: 8),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Colors.white,
+                  ),
+            )
+          ],
+        ),
+        const SizedBox(height: 12),
+        Text(
+          content,
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                color: Colors.white,
+              ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                _buildLocationHeader(),
-                SizedBox(height: 33),
-                _buildLocationImage(),
-                SizedBox(height: 36),
-                _buildWelcomeText(),
-                SizedBox(height: 20),
-                IconText(
-                  imagePath: 'assets/magic-wand.png',
-                  title: 'Location Access',
-                  content: 'Location only used when the app is open for feature enhancement',
-                ),
-                SizedBox(height: 24),
-                IconText(
-                  imagePath: 'assets/shield.png',
-                  title: 'Privacy First',
-                  content: 'No background tracking. Your location helps us tailor the app\'s features to you',
-                ),
-                SizedBox(height: 24),
-                IconText(
-                  imagePath: 'assets/padlock.png',
-                  title: 'Secure',
-                  content: 'No data collection or sharing. Your privacy is our priority',
-                ),
-                SizedBox(height: 20),
-                _buildContinueButton(),
-              ],
-            ),
-          ),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 4, 15, 18),
+        title: Text(
+          "Your Location",
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
         ),
+        centerTitle: false,
       ),
-    );
-  }
-
-  Widget _buildLocationHeader() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 48),
-      child: Align(
-        alignment: Alignment.topLeft,
-        child: Text(
-          'Your Location',
-          style: GoogleFonts.istokWeb(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildLocationImage() {
-    return Center(
-      child: SizedBox(
-        width: 350,
-        height: 239,
-        child: Image.asset('assets/document.jpg', fit: BoxFit.cover),
-      ),
-    );
-  }
-
-  Widget _buildWelcomeText() {
-    return Center(
-      child: Text(
-        'Welcome to Restazo!',
-        style: GoogleFonts.istokWeb(
-          color: Colors.white,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildContinueButton() {
-    return Center(
-      child: SizedBox(
-        width: 318,
-        height: 47,
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: const Color.fromARGB(255, 79, 79, 79),
-            shadowColor: Colors.black,
-            elevation: 4,
-          ),
-          child: Text(
-            'Continue',
-            style: GoogleFonts.istokWeb(fontSize: 16),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// IconText is a reusable widget that combines an icon with a title and content text, ensuring alignment
-class IconText extends StatelessWidget {
-  final String imagePath;
-  final String title;
-  final String content;
-
-  const IconText({
-    Key? key,
-    required this.imagePath,
-    required this.title,
-    required this.content,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 48),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start, // Align items at the start vertically
-            children: [
-              Image.asset(imagePath, width: 24, height: 24),
-              SizedBox(width: 8), // Spacing between the icon and the text
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // Align text to start horizontally
-                  children: [
-                    Text(
-                      title,
-                      style: GoogleFonts.istokWeb(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8), // Spacing between the title and content
-                    Text(
-                      content,
-                      style: GoogleFonts.istokWeb(
-                        color: Colors.white,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 44.0, left: 20, right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Center(
+                child: AspectRatio(
+                  aspectRatio: 350 / 239,
+                  child: Image.asset('assets/document.jpg', fit: BoxFit.cover),
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 12),
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 36, left: 28, right: 28),
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome to Restazo!',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  const SizedBox(height: 36),
+                  _buildSection(
+                      context,
+                      'Location Access',
+                      'Location only used when the app is open for feature enhancement',
+                      'assets/magic-wand.png'),
+                  const SizedBox(height: 24),
+                  _buildSection(
+                      context,
+                      'Privacy First',
+                      'No background tracking. Your location helps us tailor the app\'s features to you',
+                      'assets/shield.png'),
+                  const SizedBox(height: 24),
+                  _buildSection(
+                      context,
+                      'Secure',
+                      'No data collection or sharing. Your privacy is our priority',
+                      'assets/padlock.png'),
+                ],
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 56),
+              child: SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: const Color.fromARGB(255, 242, 242, 242),
+                    elevation: 0,
+                  ),
+                  child: Text(
+                    'Continue',
+                    style:
+                        GoogleFonts.istokWeb(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+
+  // Future<void> _enableLocation(BuildContext context) async {
+  //   final ph.PermissionStatus permissionGrantedResult = await ph.Permission.location.request();
+  //   if (permissionGrantedResult == ph.PermissionStatus.granted) {
+  //     final Location location = Location();
+  //     bool serviceEnabled = await location.serviceEnabled();
+  //     if (!serviceEnabled) {
+  //       serviceEnabled = await location.requestService();
+  //       if (!serviceEnabled) {
+  //         _showAlert(context, "Location Service Disabled", "Please enable location services in your device settings to continue.");
+  //         return;
+  //       }
+  //     }
+  //   } else {
+  //     _showAlert(context, "Location Permission Denied", "Please grant location permission to use this feature.");
+  //   }
+  // }
+
+  // void _showAlert(BuildContext context, String title, String content) {
+  //   showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return AlertDialog(
+  //         title: Text(title),
+  //         content: Text(content),
+  //         actions: <Widget>[
+  //           TextButton(
+  //             child: Text("OK"),
+  //             onPressed: () {
+  //               Navigator.of(context).pop();
+  //             },
+  //           ),
+  //         ],
+  //       );
+  //     },
+  //   );
+  // }
 }
