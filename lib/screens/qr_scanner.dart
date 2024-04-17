fe70c5a7277d0e579375e7ca84a041a6e31be86f
RepoRover <web.yugaitrov@gmail.com>
2024-04-17 06:28:57 +0300

    Added qr scanner files

---
+++ lib/screens/qr_scanner.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QrScannerScreen extends StatefulWidget {
+  const QrScannerScreen({super.key});
+
+  @override
+  State<QrScannerScreen> createState() => _QrScannerScreenState();
+}
+
+class _QrScannerScreenState extends State<QrScannerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
