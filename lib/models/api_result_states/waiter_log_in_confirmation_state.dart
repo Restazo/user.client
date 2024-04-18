f26019ac74288c57b392a8b08485bdc4f96c5310
RepoRover <web.yugaitrov@gmail.com>
2024-04-18 16:46:12 +0300

    Deleted unnecessary file

--- lib/models/api_result_states/waiter_log_in_confirmation_state.dart
+++
@@ -1,5 +0,0 @@
-import 'package:restazo_user_mobile/helpers/api_result.dart';
-
-class WaiterLogInConfirmationState extends APIServiceResult<String> {
-  const WaiterLogInConfirmationState({super.data, super.errorMessage});
-}
