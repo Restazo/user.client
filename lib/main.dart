d4ade08594603a6319cd7b126d80c74f8d84b336
RepoRover <web.yugaitrov@gmail.com>
2024-04-28 14:11:39 +0300

    presentation setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:restazo_user_mobile/app_block/theme.dart';
+import 'package:restazo_user_mobile/env.dart';
 import 'package:restazo_user_mobile/helpers/env_check.dart';
 import 'package:restazo_user_mobile/router/app_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   checkEnv();
+
+  // For presentation
+  // final storage = FlutterSecureStorage();
+  // final prefs = await SharedPreferences.getInstance();
+
+  // await prefs.remove(interactedKeyName);
+  // await storage.delete(key: deviceIdKeyName);
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
